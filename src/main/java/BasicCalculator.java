967f0af5150db9fd8340e77c0a29e56851979576
Justin Pacis <jpacis08@gmail.com>
2026-04-14 10:28:33 -0700

    Exercise 3, Pg. 87

--- src/main/java/BasicCalculator.java
+++ src/main/java/BasicCalculator.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class basicCalculator {
+    public class BasicCalculator {
+        public static void main(String[] args) {
+
+        Scanner scanner =  new Scanner(System.in);
+
+
+        System.out.print ("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print ("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println();
+        System.out.println("Possible calculations: ");
+        System.out.println("(A)dd");
+        System.out.println("(S)ubtract");
+        System.out.println("(M)ultiply");
+        System.out.println("(D)ivide");
+        System.out.println();
+        System.out.print ("Select any option above: ");
+
+        String option = scanner.next();
+        if (option.equals("A")){
+            System.out.println("Total is: " + (num1 + num2));
+        }
+        else if (option.equals("S")){
+            System.out.println("Total is: " + (num1 - num2));
+        }
+        else if (option.equals("M")){
+            System.out.println("Total is: " + (num1 * num2));
+        }
+        else if (option.equals("D")){
+            System.out.println("Total is: " + (num1 / num2));
+        }
+        else {
+            System.out.println("Invalid option, please try again");
+        }
+
+    }
 }
